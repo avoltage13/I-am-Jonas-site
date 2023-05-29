5b2fe8fe818e9699ec65634c34061489a3ed6283
Anton_Chorniy <81105735+avoltage13@users.noreply.github.com>
2023-05-29 03:52:00 +0300

    Add uncompressed script.js

---
+++ source/script.js
@@ -0,0 +1,76 @@
+"use strict";
+//_________________________________________________
+
+//script for no reload page when press empty links
+
+document.addEventListener("click", (noReload) => {
+    if (noReload.target.matches("[class$='__link']")) {
+        const link = noReload.target.closest('a');
+        if (link && !link.getAttribute('href')) {
+            noReload.preventDefault();
+        }
+    }
+});
+
+//script for navigation
+
+function scrollToElement(className) {
+    const element = document.querySelector('.' + className);
+    element.scrollIntoView({ behavior: 'smooth' });
+}
+
+document.addEventListener("click", (navigation) => {
+    if (navigation.target.matches('.button-work')) {
+        scrollToElement('images__title');
+    }
+    if (navigation.target.matches('.button-about')) {
+        scrollToElement('about__title');
+    }
+    if (navigation.target.matches('.button-contact')) {
+        scrollToElement('email');
+    }
+    if (navigation.target.matches('.button-home')) {
+        scrollToElement('header');
+    }
+});
+
+//goTop button
+
+const upBtn = document.querySelector('.gotop__arrow');
+let prevScrollPos = window.scrollY
+let timeoutId;
+
+window.addEventListener("scroll", trackScroll);
+window.addEventListener('scroll', function () {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(checkScrollPos, 2000);
+});
+
+function trackScroll() {
+    const currentScrollPos = window.scrollY;
+    const coord = document.documentElement.clientHeight;
+    if ((currentScrollPos > coord) && (Math.abs(currentScrollPos - prevScrollPos) >= 450)) {
+        upBtn.classList.add("gotop__arrow--show");
+    }
+    else {
+        upBtn.classList.remove("gotop__arrow--show");
+    }
+}
+
+function checkScrollPos() {
+    const currentScrollPos = window.scrollY;
+    const coord = document.documentElement.clientHeight;
+    if ((currentScrollPos !== prevScrollPos) && (currentScrollPos > coord)) {
+        upBtn.classList.remove("gotop__arrow--show");
+    }
+    prevScrollPos = currentScrollPos;
+    timeoutId = setTimeout(checkScrollPos, 2000);
+}
+
+upBtn.addEventListener("click", function goTop() {
+    if (window.scrollY > 0) {
+        upBtn.classList.remove("gotop__arrow--show");
+        window.scrollBy(0, -60);
+        setTimeout(goTop, 0);
+    }
+});
